ca7b7217d2e45d56c2d4869790d3ee5c532345b6
JavierOD <jjod@outlook.es>
2024-08-01 15:34:57 -0600

    Creacion Clase User
    Se crea paquete y clase user, con sus respectivos atributos, constructores y metodos

---
+++ User/user.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+/**
+ *
+ * @author javierortizdobles
+ */
+public class user {
+    private String id;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public user(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public user() {
+        this("","","");
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}
